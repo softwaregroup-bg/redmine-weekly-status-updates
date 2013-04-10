f871faf1851f66f43cbf094e308cbb1aa40d9bc2
zetxx <opensuser@gmail.com>
2013-04-10 18:58:21 +0300

    started new branch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,13 +9,3 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
-//= require plugins
-//= require jquery
-//= require jquery_ujs
-//= require underscore
-//= require backbone
-//= require backbone_rails_sync
-//= require backbone_datalink
-//= require backbone/test1
-//= require_tree .
